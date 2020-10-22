2d2d312701065cb6aa7be22d8f0e31b5e6a38191
Muhammad Usman <Hazeral@users.noreply.github.com>
2020-10-22 20:42:41 +0100

    Fixed comments

--- Main.cs
+++ Main.cs
@@ -6,12 +6,8 @@ public class Main : Node2D
 	// Used to determine which slot is selected by the user
 	// and show the respective sprites
 	private int selectedSlot = 1;
-	// When this is set to true the player won't be able to select
-	// any slots, this is used when a round has concluded
-	// or when the bot is selecting its slot
 	private bool GamePause = false;
-	// A 2 second timer before clearing the board after a game
-	// has concluded
+	
 	Godot.Timer cbTimer;
 	// The clear board function uses this to check which conclusion
 	// message to hide, the possible conditions are "Win", "Loss"
@@ -42,7 +38,6 @@ public class Main : Node2D
 	///<summary> This function runs once at the beginning of the game</summary>
 	public override void _Ready()
 	{
-		// Initiating timer
 		cbTimer = GetNode<Godot.Timer>("Timers/ClearBoardTimer");
 		cbTimer.Connect("timeout", this, nameof(ClearBoardAndTimeout));
 		// Making the selection sprite visible on the first slot
@@ -57,7 +52,6 @@ public class Main : Node2D
 		if (Input.IsActionJustReleased("ui_accept") && !GamePause) {
 			// Checks to see if the selected slot has already been taken
 			if (board[selectedSlot - 1] == "") {
-				// Setting the slot to a cross
 				board[selectedSlot - 1] = "X";
 				GetNode<CanvasItem>($"Crosses/Cross_{selectedSlot}").Visible = true;
 				
@@ -65,25 +59,17 @@ public class Main : Node2D
 				// a draw (board is full but no winners)
 				int won = CheckWin("Crosses", "Cross");
 				if (won != -1) {
-					// Increments score
 					AddScore("Player");
-					
-					// Makes a green streak visible over the pattern which won
+
 					GetNode<CanvasItem>($"Streaks/Streak_{(won + 1)}").Visible = true;
 					
-					// Displays a win message then initiates the
-					// clearboard timer and pauses the game
 					condition = "Win";
-					// Plays sound effect
 					SFX("Conclude");
 					cbTimer.Start();
 					GetNode<CanvasItem>("Messages/Win").Visible = true;
 					GamePause = true;
 				} else if (CheckDraw()) {
-					// Checks if there's a draw then displays a draw message
-					// and initiates clearboard timer and pauses the game
 					condition = "Draw";
-					// Plays sound effect
 					SFX("Conclude");
 					cbTimer.Start();
 					GetNode<CanvasItem>("Messages/Draw").Visible = true;
@@ -95,37 +81,24 @@ public class Main : Node2D
 				// isn't concluded
 				if (condition == null) {
 					BotSelect();
-					// Plays sound effect
 					SFX("Select");
 				}
 			}
 		}
 		
-		// If the right arrow is pressed then move the selection to the right
-		// also checks if the current slot is less than 9
-		// since the selection can not move past 9
+		// MOVEMENT 
+		
 		else if (Input.IsActionJustReleased("ui_right") && selectedSlot < 9) {
 			selectedSlot++;
-			// Plays sound effect
 			SFX("MoveSelection");
 			UpdateSelection();
 		}
-		
-		// If the left arrow is pressed then move the selection to the left
-		// also checks if the current slot is bigger than 1
-		// since the selection can not move before 1
 		else if (Input.IsActionJustReleased("ui_left") && selectedSlot > 1)
 		{
 			selectedSlot--;
-			// Plays sound effect
 			SFX("MoveSelection");
 			UpdateSelection();
 		}
-		
-		// If the down arrow is pressed then move the selection down
-		// also checks if the current slot is under 7
-		// meaning that the current selection is not on the last row
-		// since the selection is unable to move further than the last row
 		else if (Input.IsActionJustReleased("ui_down") && selectedSlot < 7)
 		{
 			selectedSlot += 3;
@@ -133,11 +106,6 @@ public class Main : Node2D
 			SFX("MoveSelection");
 			UpdateSelection();
 		}
-		
-		// If the up arrow is pressed then move the selection up
-		// also checks if the current slot is over 3
-		// meaning that the current selection would already be at least on the
-		// second row, as the selection can't move up if it's on the first row
 		else if (Input.IsActionJustReleased("ui_up") && selectedSlot > 3)
 		{
 			selectedSlot -= 3;
@@ -155,7 +123,6 @@ public class Main : Node2D
 
 	///<summary> This function hides all selections sprites and makes the sprite for the current selection visible</summary>
 	private void UpdateSelection() {
-		// Loops over each slot sprite
 		for (int i = 1; i < 10; i++) {
 			// If it is corresponding sprite for the current selection it will
 			// make it visible
@@ -170,17 +137,12 @@ public class Main : Node2D
 	
 	///<summary> This is used to clear a board after a game concludes</summary>
 	private void ClearBoard() {
-		// Loops over each slot in board
 		for (int i = 1; i < 10; i++) {
-			// Makes each slot in the board empty
 			board[i - 1] = "";
 			
-			// Hides all streaks
 			if ((i - 1) < 8) {
 				GetNode<CanvasItem>($"Streaks/Streak_{i}").Visible = false;
 			}
-			
-			// Hides each note and cross
 			GetNode<CanvasItem>($"Crosses/Cross_{i}").Visible = false;
 			GetNode<CanvasItem>($"Notes/Note_{i}").Visible = false;
 		}
@@ -196,7 +158,6 @@ public class Main : Node2D
 		// List of patterns which the node/type matches
 		bool[] Matches = new bool[8];
 		
-		// Loops over each win pattern
 		for (int i = 0; i < 8; i++) {
 			// Checks if the node/type is matching all 3 spots in the pattern
 			Matches[i] = Selected(node, $"{type}_{WinPatterns[i, 0]}") &&
@@ -204,24 +165,16 @@ public class Main : Node2D
 				Selected(node, $"{type}_{WinPatterns[i, 2]}");
 		}
 		
-		// Returning which pattern the node/type matched
 		return Array.IndexOf(Matches, true);
 	}
 
 	///<summary> The code for determining which slot the bot will select</summary>
 	private void BotSelect() {
-		// Pauses the game so the player is unable to choose any slots during
-		// this period
 		GamePause = true;
-		// This variable is used to determine whether a pattern has been found
-		// where the player is almost about to win (the player has 2/3 slots
-		// for the pattern)
+
 		bool found = false;
 		
-		// Loops over each win pattern
 		for (int i = 0; i < 8; i++) {
-			// This will not run for the remaining loop iterations if a pattern
-			// has been found which the player has almost completed
 			if (!found) {
 				
 				// List of slots the player has selected from the
@@ -246,25 +199,15 @@ public class Main : Node2D
 				// Checks if out of the pattern slots, the bot has selected
 				// 2/3 and the third slot is empty
 				if (Array.FindAll(NStreaks, s => s == false).Length == 1 && board[(WinPatterns[i, Array.IndexOf(NStreaks, false)] - 1)] == "") {
-					// Increments score
 					AddScore("Bot");
 					
-					// Takes the slot and makes the note visible there
 					board[(WinPatterns[i, Array.IndexOf(NStreaks, false)] - 1)] = "O";
 					GetNode<CanvasItem>($"Notes/Note_{(WinPatterns[i, Array.IndexOf(NStreaks, false)])}").Visible = true;
 					
-					// Sets found to true so the code is not run for the 
-					// remaining iterations
 					found = true;
-					// A streak is displayed over the winpattern
 					GetNode<CanvasItem>($"Streaks/Streak_{(CheckWin("Notes", "Note") + 1)}").Visible = true;
-					// Game is concluded and condition is set to Loss (because
-					// the player has lost)
 					condition = "Loss";
-					// Plays sound effect
 					SFX("Conclude");
-					// Timer is started to clear the board and a Loss message is
-					// shown
 					cbTimer.Start();
 					GetNode<CanvasItem>("Messages/Loss").Visible = true;
 				}
@@ -275,11 +218,9 @@ public class Main : Node2D
 					// player from winning
 					board[(WinPatterns[i, Array.IndexOf(CStreaks, false)] - 1)] = "O";
 					GetNode<CanvasItem>($"Notes/Note_{(WinPatterns[i, Array.IndexOf(CStreaks, false)])}").Visible = true;
-					// found is set to true again to prevent it from iterating
+
 					found = true;
 					
-					// Bot checks to see if it has won the game or if the game
-					// is a draw
 					int won = CheckWin("Notes", "Note");
 					// Won will return either an index to a winpattern if the
 					// bot has won or it will return -1 if no pattern was found
@@ -287,31 +228,22 @@ public class Main : Node2D
 					// winpattern which the bot matches) it will conclude the 
 					// game and the player will lose
 					if (won != -1) {
-						// Increments score
 						AddScore("Bot");
-						
-						// Plays sound effect
 						SFX("Conclude");
-					
-						// Displays streak sprite and concludes the game as a
-						// loss
+
 						GetNode<CanvasItem>($"Streaks/Streak_{(won + 1)}").Visible = true;
 						condition = "Loss";
 						
-						// Starts clearboard timer and shows the Loss message
+
 						cbTimer.Start();
 						GetNode<CanvasItem>("Messages/Loss").Visible = true;
 					
 					// Checks if board is full (draw)
 					} else if (CheckDraw()) {
-						// Condition is set to draw
 						condition = "Draw";
-						
-						// Plays sound effect
+
 						SFX("Conclude");
 						
-						// Clearboard timer is initiated and draw message is
-						// shown
 						cbTimer.Start();
 						GetNode<CanvasItem>("Messages/Draw").Visible = true;
 					}
@@ -323,58 +255,38 @@ public class Main : Node2D
 			// it checks whether i is 7 to make sure this is the final pattern
 			// which was not found
 			if (!found && i == 7) {
-				// A random index is chosen from the board
 				Random rand = new Random();  
 				int index = rand.Next(board.Length);
 				// This bool determines whether an empty spot has been found
 				// and if the loop should stop
 				bool foundRSpot = false;
 				
-				// Loops until it finds an empty spot on the board
 				do {
-					// If the slot is not empty it chooses a new random index
 					if (board[index] != "") {
 						index = rand.Next(board.Length);
 					} else {
-						// If the slot is empty then it plots a note there
 						board[index] = "O";
 						GetNode<CanvasItem>($"Notes/Note_{index + 1}").Visible = true;
 						
-						// FoundRSpot is set to true to end the loop
 						foundRSpot = true;
 						
-						// Checks if bot has won the game
 						int won = CheckWin("Notes", "Note");
 						if (won != -1) {
-							// Increments score
 							AddScore("Bot");
-							
-							// Plays sound effect
 							SFX("Conclude");
-							
-							// If won, places a streak and concludes the game
-							// with a Loss (for the player)
 							GetNode<CanvasItem>($"Streaks/Streak_{(won + 1)}").Visible = true;
 							condition = "Loss";
 							
-							// The clearboard timer is started and a Loss
-							// message is displayed
 							cbTimer.Start();
 							GetNode<CanvasItem>("Messages/Loss").Visible = true;
 							
-						// If the bot has not won it checks for a draw
 						} else if (CheckDraw()) {
-							// Condition is set to draw and the clearboard timer
-							// is started then a draw message is displayed
 							condition = "Draw";
-							// Plays sound effect
 							SFX("Conclude");
 							cbTimer.Start();
 							GetNode<CanvasItem>("Messages/Draw").Visible = true;
 						}
 					}
-					
-				// Runs until an empty random spot has been found
 				} while (foundRSpot == false);
 			}
 		}
@@ -386,15 +298,11 @@ public class Main : Node2D
 	
 	///<summary> Clears board and hides all messages</summary>
 	private void ClearBoardAndTimeout() {
-		// Clears all the notes and crosses
 		ClearBoard();
 		
-		// Hides the condition message
 		GetNode<CanvasItem>($"Messages/{condition}").Visible = false;
-		// Stops the timer so the clearboard function isn't looped every
-		// 2 seconds
+
 		cbTimer.Stop();
-		// Unpauses the game and sets the games condition back to null
 		GamePause = false;
 		condition = null;
 	}
@@ -402,10 +310,9 @@ public class Main : Node2D
 	///<summary> Increments score for a user</summary>
 	private void AddScore(string user) {
 		// User should either be "Player" or "Bot"
-		// Parses current score of user as an int
+		
 		int currentScore = int.Parse(GetNode<Label>($"Scores/{user}").Text);
 		
-		// Increments current score and replaces the score text for user
 		currentScore++;
 		GetNode<Label>($"Scores/{user}").Text = currentScore.ToString();
 	}
@@ -414,7 +321,6 @@ public class Main : Node2D
 	private void SFX(string sound) {
 		// Sound should be "MoveSelection", "Select" or "Conclude"
 		
-		// Plays the sound
 		GetNode<AudioStreamPlayer>($"Sounds/{sound}").Playing = true;
 	}
 }
